bfba39d476fc487f5d81e2729230994c6d2cc89d
Amir Mohammad tahsiri <32165355+mamadgit@users.noreply.github.com>
2026-02-17 19:21:03 +0330

    Added 0.0.0.0 to open link through IP

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/', (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+const PORT = process.env.PORT || 3000; 
+app.listen(PORT, "0.0.0.0",() => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
